ec579d88ba5eddbd75b7d2c1c388855c5d6b630f
ztlab111 <dineshp@zignuts.com>
2023-05-10 10:09:26 +0530

    registration logic added

--- app/src/main/java/com/example/fooddonationapp/ui/auth/login/LoginFragment.kt
+++ app/src/main/java/com/example/fooddonationapp/ui/auth/login/LoginFragment.kt
@@ -5,18 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.fooddonationapp.R
+import com.example.fooddonationapp.databinding.FragmentLoginBinding
+import com.example.fooddonationapp.databinding.FragmentOnBoardingBinding
 
 class LoginFragment : Fragment() {
-
+    private var _binding : FragmentLoginBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        _binding = FragmentLoginBinding.inflate(inflater,container,false)
 
+        binding.signup.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegistrationFragment())
+        }
 
-        return inflater.inflate(R.layout.fragment_login, container, false)
+//        setUpUi()
+//        setOnClicks()
+        return binding.root
     }
 
 
--- app/src/main/java/com/example/fooddonationapp/ui/auth/registration/RegistrationFragment.kt
+++ app/src/main/java/com/example/fooddonationapp/ui/auth/registration/RegistrationFragment.kt
@@ -5,17 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import com.example.fooddonationapp.R
+import com.example.fooddonationapp.databinding.FragmentOnBoardingBinding
+import com.example.fooddonationapp.databinding.FragmentRegistrationBinding
 
 
 class RegistrationFragment : Fragment() {
-
+    private var _binding : FragmentRegistrationBinding? = null
+    private val binding get() = _binding!!
+    var types = arrayOf("Nadiad","Surat","Bharuch","Baroda","Ahmedabad")
+    lateinit var typeSpin : Spinner
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false)
+
+        _binding = FragmentRegistrationBinding.inflate(inflater,container,false)
+//        setUpUi()
+
+//        setOnClicks()
+        typeSpin = binding.spinner
+
+        val typeAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, types)
+        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        typeSpin!!.setAdapter(typeAdapter)
+
+
+//        binding.spinner.onItemSelectedListener = object :  AdapterView.OnItemSelectedListener {
+//            override fun onItemSelected(
+//                parent: AdapterView<*>?,
+//                view: View?,
+//                position: Int,
+//                id: Long
+//            ) {
+//
+//                var v = binding.editText.text.toString().toDoubleOrNull()
+//                viewmodeltmp.selectedUnit = types[position].first().uppercase()
+//                viewmodeltmp.calculate(v?:0.0)
+//                //binding.tmpshow = viewmodeltmp
+//                Toast.makeText(applicationContext,"${types[position]}", Toast.LENGTH_SHORT).show()
+//
+//                //getString(R.string.selected_item) + " " + "" + types[position],Toast.LENGTH_SHORT).show()
+//            }
+//            override fun onNothingSelected(parent: AdapterView<*>?) {
+//                TODO("Not yet implemented")
+//            }
+//        }
+        return binding.root
+
     }
 
 
