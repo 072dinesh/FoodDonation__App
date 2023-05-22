b5e9f75b8de5ab7181df277798fca3dd7684aaa4
HarshMZignuts <122282938+HarshMZignuts@users.noreply.github.com>
2023-05-22 12:47:21 +0530

    View Pagger Constrain set

---
+++ app/src/main/java/com/example/fooddonationapp/ui/tabs/donor/recent/RecentDonorTabAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.fooddonationapp.ui.tabs.donor.recent
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fooddonationapp.R
+import com.example.fooddonationapp.databinding.ListDonorRecentBinding
+import com.example.fooddonationapp.databinding.NgoItemHistoryListBinding
+import com.example.fooddonationapp.model.Request
+import com.example.fooddonationapp.utils.DiffUtilExt
+
+class RecentDonorTabAdapter : RecyclerView.Adapter<RecentDonorTabAdapter.MyViewHolder>() {
+    private var requestList = emptyList<Request>()
+    class MyViewHolder(private val binding : ListDonorRecentBinding):RecyclerView.ViewHolder(binding.root) {
+        fun bind(currentItem : Request){
+           binding.requestData=currentItem
+            binding.tvListRecentDonorNgoName.text = itemView.context.getString(R.string.request_from_format,currentItem.ngoname)
+            binding.tvListRecentDonorQuantity.text = itemView.context.getString(R.string.request_quantity_format,currentItem.quantity)
+            binding.tvListRecentDonorLocation.text = itemView.context.getString(R.string.request_location_format,currentItem.location)
+            binding.tvListRecentDonorNgoPhone.text = itemView.context.getString(R.string.request_phone_format,currentItem.phoneno)
+        }
+        companion object{
+            fun from(parent: ViewGroup) : MyViewHolder{
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ListDonorRecentBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+      return MyViewHolder.from(parent)
+    }
+
+    override fun getItemCount(): Int {
+        return requestList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem = requestList.getOrNull(position)
+        currentItem?.let {
+            holder.bind(it)
+        }
+    }
+
+    fun setData(Result : List<Request>){
+        val diffUtil = DiffUtilExt(requestList,Result)
+        val diffUtilResult  = DiffUtil.calculateDiff(diffUtil)
+        requestList = Result
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+}
--- app/src/main/java/com/example/fooddonationapp/ui/tabs/donor/recent/RecentDonorTabFragment.kt
+++ app/src/main/java/com/example/fooddonationapp/ui/tabs/donor/recent/RecentDonorTabFragment.kt
@@ -5,23 +5,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.fooddonationapp.R
 import com.example.fooddonationapp.databinding.FragmentOnBoardingBinding
 import com.example.fooddonationapp.databinding.FragmentRecentDonorTabBinding
+import com.example.fooddonationapp.model.Request
+import com.google.firebase.firestore.FirebaseFirestore
+import okhttp3.internal.userAgent
+import timber.log.Timber
 
 
 class RecentDonorTabFragment : Fragment() {
     private var _binding : FragmentRecentDonorTabBinding? = null
     private val binding get() = _binding!!
+    private lateinit var requestArrayList : ArrayList<Request>
+    private lateinit var recentDonorAdapter : RecentDonorTabAdapter
+    private lateinit var db : FirebaseFirestore
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentRecentDonorTabBinding.inflate(inflater,container,false)
+        db = FirebaseFirestore.getInstance()
+        binding.donorRecentRecycler.layoutManager = LinearLayoutManager(requireContext())
+        requestArrayList = arrayListOf()
 
+
+        recentList()
+        binding.donorRecentRecycler.setHasFixedSize(true)
         return binding.root
     }
+private fun recentList(){
+    db.collection("Request").get().addOnSuccessListener {
+        if (!it.isEmpty){
+            for (data in it.documents){
 
+                val request : Request? = data.toObject(Request::class.java)
+                if (request != null){
+
+                    requestArrayList.add(request)
+                    Timber.e("${requestArrayList.size.toString()}")
+                }
+            }
+            recentDonorAdapter = RecentDonorTabAdapter()
+            binding.donorRecentRecycler.adapter = recentDonorAdapter
+            recentDonorAdapter.setData(requestArrayList)
+
+        }
+    }
+
+}
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
 }
